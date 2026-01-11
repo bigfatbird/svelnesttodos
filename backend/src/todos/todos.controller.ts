7b62a8b5556c24ae1c089635b3ab5d45dc5b2ee1
Marco Dahms <dahmsdahmsdahms@gmail.com>
2026-01-11 11:29:35 +0100

    Implement Todos service with CRUD support and integrate with controller

--- backend/src/todos/todos.controller.ts
+++ backend/src/todos/todos.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
 export class TodosController {
+  constructor(private todosService: TodosService) {}
+
   // GET /todos
   @Get()
   findAll() {
-    return [];
+    return this.todosService.findAll();
   }
+
   // GET /todos/:id
   @Get(':id')
   findOne(@Param('id') id: string) {
     return { id };
   }
+
   // POST /todos
   @Post()
   create(@Body() createTodoDto: CreateTodoDto) {
@@ -21,6 +37,17 @@ export class TodosController {
       description: createTodoDto.description,
     };
   }
+
   // PUT /todos/:id
+  @Put('id')
+  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+    return { id, ...updateTodoDto };
+  }
+
   // DELETE /todos/:id
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return { id };
+  }
 }
